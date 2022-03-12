eb4b1b7a40be34ae8cc64ed475be8461ea1af14f
Pirity Tamas <pirity.tom@gmail.com>
2022-03-12 15:08:23 +0100

    Változtatások

--- src/hu/progmatic/Lesson_2022_03_12.java
+++ src/hu/progmatic/Lesson_2022_03_12.java
@@ -1,8 +1,23 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class Lesson_2022_03_12 {
     public static void main(String[] args) {
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Kérem az évszámot.");
+        int evszam = sc.nextInt();
+        sc.nextLine();
+        boolean szokoevE = false;
 
+        if (1582 <= evszam) {
+            if (evszam %400 == 0) {
+                szokoevE = true;
+            } else if (evszam %4 == 0 && evszam %100 != 0) {
+                szokoevE = true;
+            }
+        }
+
+        System.out.println("Ez az év " + (szokoevE ? "" : "nem ") + "szökőév.");
     }
 }
