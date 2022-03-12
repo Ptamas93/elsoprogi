4d14da4942586747a5bfe57ed995e9c25e14b26b
Pirity Tamas <pirity.tom@gmail.com>
2022-03-12 17:35:24 +0100

    Változtatások

--- src/hu/progmatic/Taskeasy_02.java
+++ src/hu/progmatic/Taskeasy_02.java
@@ -1,4 +1,16 @@
 package hu.progmatic;
 
+import java.util.Scanner;
+
 public class Taskeasy_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Adj meg egy számot!");
+        int a = sc.nextInt();
+
+        if (a > 0) {
+            a--;
+        }
+        System.out.println("A szám értéke: " + a);
+    }
 }
